6a1c1fc3abf4eab933821b5ca1314b4d540de4d4
UpsetChap <pirateluvver@hotmail.com>
2025-11-19 23:42:53 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+author "UpsetChap"
+version "1.0"
+
+files {
+	'handling.meta'
+}
+
+resource_type 'gametype' { name = 'UC Handling' }
+
+data_file 'HANDLING_FILE' 'handling.meta'
